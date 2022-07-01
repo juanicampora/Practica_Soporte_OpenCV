90ff459fef2535b12286c9bb960119f7a1815069
Juani <juanicampora@gmail.com>
2022-06-30 22:47:58 -0300

    Tarea de 30_06

--- 10-Guardar5SegsVideo.py
+++ 10-Guardar5SegsVideo.py
@@ -6,12 +6,24 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
+#SALIDA (Graba video)
+frame_width = int(cap.get(3))   #OBTENGO ANCHO
+frame_height = int(cap.get(4))  #OBTENGO ALTO
+out = cv2.VideoWriter('videograbado.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame_width,frame_height))
+
 itera=True
 temporizando=False
 numeronombre=0
+
+xiniciorect=(frame_width-200)
+xfinrect=frame_width
+#xiniciorect=600
+#xfinrect=800
+
+
 while True:
     success, image = cap.read()
-    image=cv2.resize(image, (800, 700))
+    #image=cv2.resize(image, (800, 700))        #Ajustar tamaño imagen
     image=cv2.flip(image,1)
     original = deepcopy(image)
     imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -24,16 +36,16 @@ while True:
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                if ((600<cx<800) and (0<cy<200)):
+                if ((xiniciorect<cx<xfinrect) and (0<cy<200)):
                     if itera:
-                        cv2.rectangle(image,(600,0),(800,200),(0,255,0),-1)
+                        cv2.rectangle(image,(xiniciorect,0),(xfinrect,200),(0,255,0),-1)
                     afuera=False
             #mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
             mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS,mpDraw.DrawingSpec(color=(57,110,151), thickness=2, circle_radius=2),
             mpDraw.DrawingSpec(color=(82,155,212), thickness=2, circle_radius=2))
         if afuera:
-            cv2.rectangle(image,(600,0),(800,200),(0,0,255),2)
-    cv2.rectangle(original, (600, 0), (800, 200), (90, 90, 81), 2)
+            cv2.rectangle(image,(xiniciorect,0),(xfinrect,200),(0,0,255),2)
+    cv2.rectangle(original, (xiniciorect, 0), (xfinrect, 200), (90, 90, 81), 2)
     cv2.imshow("Analisis", image)
     cv2.imshow("Original", original)
     tecla= cv2.waitKey(1)
@@ -41,11 +53,9 @@ while True:
         break
     elif tecla==32:
         iniciotemp=time.time()
+        out.write(original)
         temporizando=True
-    if temporizando and (time.time()-iniciotemp)>=5:
-        numeronombre+=1
-        numeronombreconcero=str(numeronombre).rjust(5, '0')
-        nombre=f"imagen {numeronombreconcero}.png"
-        cv2.imwrite(str(nombre), original)
-        cv2.imshow('infraccion ', original)
+    if temporizando and (time.time()-iniciotemp)<=5:
+        out.write(original)
+    elif temporizando and (time.time()-iniciotemp)>5:
         temporizando=False
